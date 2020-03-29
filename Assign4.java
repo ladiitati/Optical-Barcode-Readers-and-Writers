dff50a7125a0a277f0197e2ea1ffdc636dd978de
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-03-28 19:21:11 -0700

    fix variable names

--- Assign4.java
+++ Assign4.java
@@ -93,17 +93,17 @@ class BarcodeImage implements Cloneable {
         return imageData;
     }
 
-    public boolean getPixel(int col, int row) {
+    public boolean getPixel(int row, int col) {
         try {
-            return imageData[col][row];
+            return imageData[row][col];
         } catch (Exception e) {
             return false;
         }
     }
 
-    public void setPixel(int col, int row, boolean value) {
+    public void setPixel(int row, int col, boolean value) {
         try {
-            this.imageData[col][row] = value;
+            this.imageData[row][col] = value;
         } catch (Exception e) {
             System.out.println(e.toString());
         }
