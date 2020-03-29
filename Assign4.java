2d166e3fcd2ce40567ae3c23bab4dda3dae46548
Row <62412509+rtterr@users.noreply.github.com>
2020-03-28 19:05:19 -0700

    initial commit

--- Assign4.java
+++ Assign4.java
@@ -101,6 +101,7 @@ class BarcodeImage implements Cloneable {
     }
 
     public void setPixel(int col, int row, boolean value) {
+//        System.out.println("col " + col + " row " + row);
         try {
             this.imageData[col][row] = value;
         } catch (Exception e) {
@@ -126,4 +127,73 @@ class BarcodeImage implements Cloneable {
             System.out.println(Arrays.toString(image[i]));
         }
     }
+}
+
+class DataMatrix implements BarcodeIO {
+	  public static final char BLACK_CHAR = '*';
+	  public static final char WHITE_CHAR = ' ';
+	  private BarcodeImage image;
+	  private String text;
+	  private int actualWidth;
+	  private int actualHeight;
+	   
+    public boolean scan(BarcodeImage bc) {
+    	cleanImage();
+    	return true;
+    	}
+
+    public boolean readText(String text) {return true;}
+
+    public boolean generateImageFromText() {return true;}
+
+    public boolean translateImageToText() {return true;}
+
+    public void displayTextToConsole() {}
+
+    public void displayImageToConsole() {}
+    
+    private int computeSignalWidth(){
+    	int width = 0;
+    	//INCORRECT; change to look at bottom row
+    	for (int i = 0; i < BarcodeImage.MAX_WIDTH; i++) {
+    		if (image.getPixel(i, 0)) {
+    			width++;
+    		}
+    	}
+    	return width;
+    }
+    private int computeSignalHeight(){
+    	int height = 0;
+    	for (int j = 0; j < BarcodeImage.MAX_HEIGHT; j++) {
+    		if (image.getPixel(0, j)) {
+    			height++;
+    		}
+    	}
+    	return height;
+    }
+    
+    public void displayRawImage(){}
+    private void clearImage(){}
+    
+    private void cleanImage() {}
+    private void moveImageToLowerLeft(){
+    	//find max offset (blank spaces to bottom and right)
+    	//locate first instance of existing pixel within barcode
+    	//find how many movements it took to reach that (offset)
+    	for (int i = 0; i < BarcodeImage.MAX_HEIGHT; i++) {
+    		if (image.getPixel(col, row)) {
+    			
+    		}
+    	}
+    	//use shiftImageDown and shiftImageLeft to move that number of offsets
+    }
+    
+    private void shiftImageDown(int offset){
+    	//move matrix down (offset number) of rows
+    	//make another helper function to prevent moving too many rows?
+    }
+    private void shiftImageLeft(int offset){
+    	//move matrix left (offset number) of columns
+    }
+
 }
