1a7b132615bf971c3183cb40eb5fc2d5e15db59b
ladiitati <tatianafadams96@gmail.com>
2020-03-29 15:37:31 -0700

    true or false

--- Assign4.java
+++ Assign4.java
@@ -1,129 +1,104 @@
-import java.util.Arrays;
 
-public class Assign4 {
+public class assign4 {
+
     public static void main(String[] args) {
-        testPhaseTwo();
-    }
-
-    /**
-     * testPhaseTwo is only for the purpose of testing the
-     * BarcodeImage class. Feel free to delete later.
-     */
-    public static void testPhaseTwo() {
-        String[] phaseTwoTest = new String[]{
-                "* * * * * * * * * * * * * * * * * *",
-                "*                                 *",
-                "***** ** * **** ****** ** **** **  ",
-                "* **************      *************",
-                "**  *  *        *  *   *        *  ",
-                "* **  *     **    * *   * ****   **",
-                "**         ****   * ** ** ***   ** ",
-                "*   *  *   ***  *       *  ***   **",
-                "*  ** ** * ***  ***  *  *  *** *   ",
-                "***********************************"
+        String[] sImageIn =
+        {
+           "                                               ",
+           "                                               ",
+           "                                               ",
+           "     * * * * * * * * * * * * * * * * * * * * * ",
+           "     *                                       * ",
+           "     ****** **** ****** ******* ** *** *****   ",
+           "     *     *    ****************************** ",
+           "     * **    * *        **  *    * * *   *     ",
+           "     *   *    *  *****    *   * *   *  **  *** ",
+           "     *  **     * *** **   **  *    **  ***  *  ",
+           "     ***  * **   **  *   ****    *  *  ** * ** ",
+           "     *****  ***  *  * *   ** ** **  *   * *    ",
+           "     ***************************************** ",  
+           "                                               ",
+           "                                               ",
+           "                                               "
         };
-        BarcodeImage b = new BarcodeImage(phaseTwoTest);
-        b.displayToConsole();
+        System.out.println("Start Program");
+        BarcodeImage obj1 = new BarcodeImage(sImageIn);
+
     }
-}
 
-interface BarcodeIO {
-    public boolean scan(BarcodeImage bc);
+ }
 
-    public boolean readText(String text);
-
-    public boolean generateImageFromText();
-
-    public boolean translateImageToText();
-
-    public void displayTextToConsole();
-
-    public void displayImageToConsole();
-}
-
-class BarcodeImage implements Cloneable {
+class BarcodeImage {
     public static final int MAX_HEIGHT = 30;
     public static final int MAX_WIDTH = 65;
+
     private boolean[][] imageData;
 
-    BarcodeImage(String[] strData) {
-        boolean[][] image = new boolean[MAX_HEIGHT][MAX_WIDTH];
+    BarcodeImage(){
+        imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
 
-        // check if input is valid
-        if (!checkSize(strData)) {
-            return;
-        }
-        ;
-        this.setImageData(image);
+        for(int row = 0; row < MAX_HEIGHT; row ++){
+            for(int col = 0; col < MAX_WIDTH; col++){
+                this.imageData[row][col] = false;
+            }
+        } 
+    }
+    BarcodeImage(String[] strData){
 
-        for (int i = 0; i < strData.length; i++) {
-            /**
-             * for each character in the strings set booleans
-             * in the matrix starting at the bottom left.
-             * */
-            for (int j = 0; j < strData[i].length(); j++) {
-                if (strData[(strData.length - 1) - i].charAt(j) == '*') {
-                    setPixel((MAX_HEIGHT - 1) - i, j, true);
-                } else {
-                    setPixel((MAX_HEIGHT - 1) - i, j, false);
+        this.imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
+        int beginOfArrayRow = 0; 
+        int beginOfArrayCol  = 0;
+        int endOfArrayRow = 0;
+        int endOfArrayCol = 0;
+
+        boolean checkSize = true;
+
+        if(checkSize == true){
+        //Left Corner points   
+       // if(checkSize(strData) == true){
+
+            for(int strRow = 0; strRow < strData.length; strRow++){
+                for(int strCol = 0; strCol < strData[strRow].length(); strCol++){
+
+                    if(strData[strRow].charAt(strCol) == '*'){
+                        endOfArrayRow = strRow;
+                        endOfArrayCol = strCol;
+                    }
+
                 }
             }
-        }
-    }
 
-    // sets every value to false if no data passed in
-    BarcodeImage() {
-        boolean[][] image = new boolean[MAX_HEIGHT][MAX_WIDTH];
-        this.setImageData(image);
+            for(int strRow = strData.length - 1; strRow >= 0; strRow--){
+                for(int strCol = strData[strRow].length() - 1; strCol >= 0; strCol--){
 
-        // iterate through every column and row
-        for (int i = 0; i < MAX_HEIGHT; i++) {
-            for (int j = 0; j < MAX_WIDTH; j++) {
-                this.setPixel(i, j, false);
-            }
-        }
-    }
+                    if(strData[strRow].charAt(strCol) == '*'){
+                        beginOfArrayRow = strRow;
+                        beginOfArrayCol = strCol;
+                    }
+                }
 
-    public void setImageData(boolean[][] imageData) {
-        this.imageData = imageData;
-    }
+                }
+                for(int strRow1 = beginOfArrayRow; strRow1 <= endOfArrayRow; strRow1++){
 
-    public boolean[][] getImageData() {
-        return imageData;
-    }
+                    for(int strCol1 = beginOfArrayCol; strCol1 <= endOfArrayCol; strCol1++){
 
-    public boolean getPixel(int col, int row) {
-        try {
-            return imageData[col][row];
-        } catch (Exception e) {
-            return false;
-        }
-    }
+                       if(strData[strRow1].charAt(strCol1) == '*') {
+                          imageData[MAX_HEIGHT - strData.length + strRow1][strCol1] = true;
 
-    public void setPixel(int col, int row, boolean value) {
-        try {
-            this.imageData[col][row] = value;
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-    }
-
-    private boolean checkSize(String[] data) {
-        if (data.length <= MAX_HEIGHT) {
-            for (String datum : data) {
-                if (datum != null && datum.length() <= MAX_WIDTH) {
-                    return true;
+                       }
+                       else {
+                          imageData[MAX_HEIGHT - strData.length + strRow1][strCol1] = false;
+                       }
                 }
             }
-        }
-        return false;
-    }
+                for(int strRow = 0; strRow < strData.length - 1; strRow++){
+                   for(int strCol = 0; strCol < strData[strRow].length() - 1; strCol++){
+                       System.out.println("strData[strRow].charAt(strCol) = [" + strRow + "][" + strCol + "] " + strData[strRow].charAt(strCol) + " = " + imageData[MAX_HEIGHT - strData.length + strRow][strCol] );
 
-    public void displayToConsole() {
-        boolean[][] image = this.getImageData();
-
-        for (int i = 0; i < MAX_HEIGHT; i++) {
-            System.out.println(Arrays.toString(image[i]));
+                   }
+               }
         }
+
     }
 }
+
